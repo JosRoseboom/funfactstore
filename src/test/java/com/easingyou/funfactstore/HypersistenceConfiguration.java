625021ea1941f002a32518916c1baef49660c1d1
josroseboom <josroseboom@gmail.com>
2025-05-13 10:17:41 +0200

    Added HypersistenceOptimizer

---
+++ src/test/java/com/easingyou/funfactstore/HypersistenceConfiguration.java
@@ -0,0 +1,22 @@
+package com.easingyou.funfactstore;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.hypersistence.optimizer.HypersistenceOptimizer;
+import io.hypersistence.optimizer.core.config.JpaConfig;
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+public class HypersistenceConfiguration {
+
+    @Bean
+    public HypersistenceOptimizer hypersistenceOptimizer(
+            EntityManagerFactory entityManagerFactory) {
+        return new HypersistenceOptimizer(
+                new JpaConfig(
+                        entityManagerFactory
+                )
+        );
+    }
+}
