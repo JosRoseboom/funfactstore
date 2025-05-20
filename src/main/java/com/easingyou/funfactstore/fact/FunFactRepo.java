25e2f525906a6ae4c017497b69b129d3a4af07a9
josroseboom <josroseboom@gmail.com>
2025-05-20 09:25:47 +0200

    Added join fetch admin

--- src/main/java/com/easingyou/funfactstore/fact/FunFactRepo.java
+++ src/main/java/com/easingyou/funfactstore/fact/FunFactRepo.java
@@ -4,8 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FunFactRepo extends JpaRepository<FunFact, Long> {
 
 	Optional<FunFact> findFirstByIdNotIn(List<Long> factIds);
+
+	@Query("""
+		from FunFact ff join fetch ff.admin
+	""")
+	List<FunFact> findAllWithAdmin();
 }
