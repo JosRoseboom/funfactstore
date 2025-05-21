cf74a8666c4f7d1ee731db01da45c727977c7bd7
josroseboom <josroseboom@gmail.com>
2025-05-21 13:54:54 +0200

    Added equals and hashcode to base entity

--- src/main/java/com/easingyou/funfactstore/fact/BaseEntity.java
+++ src/main/java/com/easingyou/funfactstore/fact/BaseEntity.java
@@ -1,5 +1,7 @@
 package com.easingyou.funfactstore.fact;
 
+import org.hibernate.proxy.HibernateProxy;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,4 +17,33 @@ public class BaseEntity{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(nullable = false)
 	private Long id;
+
+	 // Equals() and hashCode() for entities should be consistent across all state transitions.
+	 //
+	 // If an identifier is known before persisting (natural id like social security number or id that is known upfront), that identifier could be used as a base for equals and hashcode.
+	 //
+	 // If no such identifier is known (the database generates it on insert), the implementation shown here could be used.
+	 //
+	 // Very good posts on this:
+	 // - https://jpa-buddy.com/blog/hopefully-the-final-article-about-equals-and-hashcode-for-jpa-entities-with-db-generated-ids/
+	 // - https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+	@Override
+	public final boolean equals(Object o) {
+		if (this == o)
+			return true;
+
+		return o instanceof BaseEntity baseEntity
+				&& this.getId() != null
+				&& this.getId().equals(baseEntity.getId())
+				&& this.getEffectiveClass() == baseEntity.getEffectiveClass();
+	}
+
+	@Override
+	public final int hashCode() {
+		return getEffectiveClass().hashCode();
+	}
+
+	private Class<?> getEffectiveClass() {
+		return this instanceof HibernateProxy hibernateProxy ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass() : getClass();
+	}
 }
---
+++ src/test/java/com/easingyou/funfactstore/fact/myfacts/IsInstanceTest.java
@@ -0,0 +1,43 @@
+package com.easingyou.funfactstore.fact.myfacts;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.easingyou.funfactstore.TestcontainersConfiguration;
+
+@Import(TestcontainersConfiguration.class)
+@ActiveProfiles("test")
+@DataJpaTest
+class IsInstanceTest {
+
+	@Autowired
+	private MyPurchasesRepo myPurchasesRepo;
+
+	@Test
+	void testHashcodeReference(){
+		final int hashCodeRef = myPurchasesRepo.getReferenceById(5L).hashCode();
+		final int hashCodeInstantiated = myPurchasesRepo.findById(5L).orElseThrow().hashCode();
+		assertEquals(hashCodeRef, hashCodeInstantiated);
+	}
+
+	@Test
+	void testEqualsRef(){
+		assertEquals(myPurchasesRepo.getReferenceById(1L), myPurchasesRepo.getReferenceById(1L));
+	}
+
+	@Test
+	void testEquals(){
+		assertEquals(myPurchasesRepo.findById(1L).orElseThrow(), myPurchasesRepo.getReferenceById(1L));
+	}
+
+	@Test
+	void testNotEquals(){
+		assertNotEquals(myPurchasesRepo.findById(1L).orElseThrow(), myPurchasesRepo.getReferenceById(2L));
+	}
+}
